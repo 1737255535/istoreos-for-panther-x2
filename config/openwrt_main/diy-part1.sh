89ee70f8bcc2184917b7a1751a613a8fa5ac6a63
cc <1737255535@users.noreply.github.com>
2026-06-03 22:26:30 +0800

    Update diy-part1.sh

--- config/openwrt_main/diy-part1.sh
+++ config/openwrt_main/diy-part1.sh
@@ -24,6 +24,6 @@ echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
+# echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+# echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
